ec8067bd19f7d25f92287a6076a1f59cc1186299
Jared Sunday <jaredpatterson07@gmail.com>
2023-04-11 15:45:35 -0700

    finished

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HookForm from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <HookForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Form.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+const HookForm = () => {
+    const [firstName, setFirstName] =useState('');
+    const [lastName, setLastName] =useState('');
+    const [email, setEmail] =useState('');
+    const [password, setPassword] =useState('');
+    const [confirmPassword, setConfirmPassword] =useState('');
+
+    return(
+        <>
+            <form >
+                <div>
+                    <label htmlFor='firstName'>First Name:</label>
+                    <input type = 'text' value = {firstName} onChange= {(e)=> setFirstName(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor='lastName'>Last Name:</label>
+                    <input type = 'text' value = {lastName} onChange= {(e)=> setLastName(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor='email'>Email:</label>
+                    <input type = 'text' value = {email} onChange= {(e)=> setEmail(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor='password'>Password:</label>
+                    <input type = 'password' value = {password} onChange= {(e)=> setPassword(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor='confirmPassword'>Confirm Password:</label>
+                    <input type = 'password' value = {confirmPassword} onChange= {(e)=> setConfirmPassword(e.target.value)}/>
+                </div>
+            </form>
+            <div className='data'>
+                <h2>Your Form Data</h2>
+                <p>First Name: {firstName}</p>
+                <p>Last Name: {lastName}</p>
+                <p>Email: {email}</p>
+                <p>Password: {password}</p>
+                <p>Confirm Password: {confirmPassword}</p>
+            </div>
+        
+        </>
+    );
+};
+export default HookForm;
